2542eadc51047d5290a5eb1cc78032633a789e9b
Taybueno9 <130675653+Taybueno9@users.noreply.github.com>
2024-05-22 11:11:37 -0300

    arquivo

---
+++ script.js
@@ -0,0 +1,5 @@
+const caixaPrincipal = document.querySelector(.caixa-principal);
+const caixaPerguntas = document.querySelector(.caixa-pergintas);
+const caixaAternativas = document.querySelector(.caixa-alternativas);
+const caixaResultado = document.querySelector(.ciaxa-resultado);
+const textoResultado = document.querySelector(.texto-resultado);
